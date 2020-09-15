879736d4a2caac31d058e5ff0f7b2e910c305b60
afroza nisha <nisha.afroza1998@gmail.com>
2020-09-15 23:29:33 +0600

    admin customize successfully

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -1,4 +1,3 @@
-
 <footer class="main-footer">
     <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">Ambala.It</a>.</strong>
     All rights reserved.
@@ -13,90 +12,93 @@
 <!-- /.control-sidebar -->
 </div>
 <!-- jQuery -->
-<script src="{{ asset('/') }}/public/plugins/jquery/jquery.min.js"></script>
+<script src="{{ asset('/') }}/backend/plugins/jquery/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="{{ asset('/') }}/public/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="{{ asset('/') }}/backend/plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
-$.widget.bridge('uibutton', $.ui.button)
+    $.widget.bridge('uibutton', $.ui.button)
+
 </script>
 <!-- Bootstrap 4 -->
-<script src="{{ asset('/') }}/public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('/') }}/backend/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- ChartJS -->
-<script src="{{ asset('/') }}/public/plugins/chart.js/Chart.min.js"></script>
+<script src="{{ asset('/') }}/backend/plugins/chart.js/Chart.min.js"></script>
 <!-- Sparkline -->
-<script src="{{ asset('/') }}/public/plugins/sparklines/sparkline.js"></script>
+<script src="{{ asset('/') }}/backend/plugins/sparklines/sparkline.js"></script>
 <!-- JQVMap -->
-<script src="{{ asset('/') }}/public/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="{{ asset('/') }}/public/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="{{ asset('/') }}/backend/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="{{ asset('/') }}/backend/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="{{ asset('/') }}/public/plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="{{ asset('/') }}/backend/plugins/jquery-knob/jquery.knob.min.js"></script>
 <!-- daterangepicker -->
-<script src="{{ asset('/') }}/public/plugins/moment/moment.min.js"></script>
-<script src="{{ asset('/') }}/public/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="{{ asset('/') }}/backend/plugins/moment/moment.min.js"></script>
+<script src="{{ asset('/') }}/backend/plugins/daterangepicker/daterangepicker.js"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="{{ asset('/') }}/public/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="{{ asset('/') }}/backend/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
 <!-- Summernote -->
-<script src="{{ asset('/') }}/public/plugins/summernote/summernote-bs4.min.js"></script>
+<script src="{{ asset('/') }}/backend/plugins/summernote/summernote-bs4.min.js"></script>
 <!-- overlayScrollbars -->
-<script src="{{ asset('/') }}/public/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="{{ asset('/') }}/backend/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset('/') }}/public/dist/js/adminlte.js"></script>
+<script src="{{ asset('/') }}/backend/dist/js/adminlte.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{ asset('/') }}/public/dist/js/pages/dashboard.js"></script>
+<script src="{{ asset('/') }}/backend/dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{ asset('/') }}/public/dist/js/demo.js"></script>
+<script src="{{ asset('/') }}/backend/dist/js/demo.js"></script>
 <!-- datatables -->
-<script src="{{ asset('/') }}/public/plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="{{ asset('/') }}/public/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="{{ asset('/') }}/backend/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="{{ asset('/') }}/backend/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
 <!-- jquery-validation -->
-<script src="{{ asset('/') }}/public/plugins/jquery-validation/jquery.validate.min.js"></script>
-<script src="{{ asset('/') }}/public/jquery-validation/additional-methods.min.js"></script>
+<script src="{{ asset('/') }}/backend/plugins/jquery-validation/jquery.validate.min.js"></script>
+<script src="{{ asset('/') }}/backend/jquery-validation/additional-methods.min.js"></script>
 <!-- jquery-sweetalert -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
 <!-- jquery-notify -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.js"></script>
 
 <script>
-$(function () {
-    $("#example1").DataTable({
-        "responsive": true,
-        "autoWidth": false,
+    $(function() {
+        $("#example1").DataTable({
+            "responsive": true
+            , "autoWidth": false
+        , });
     });
-});
-$(function () {
-    $(document).on('click', '#delete', function (e) {
-        e.preventDefault();
-        var link = $(this).attr('href');
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "Delete this data!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.value) {
-                window.location.href = link;
-                Swal.fire(
-                        'Deleted!',
-                        'Your file has been deleted.',
-                        'success'
-                        )
-            }
-        })
+    $(function() {
+        $(document).on('click', '#delete', function(e) {
+            e.preventDefault();
+            var link = $(this).attr('href');
+            Swal.fire({
+                title: 'Are you sure?'
+                , text: "Delete this data!"
+                , icon: 'warning'
+                , showCancelButton: true
+                , confirmButtonColor: '#3085d6'
+                , cancelButtonColor: '#d33'
+                , confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.value) {
+                    window.location.href = link;
+                    Swal.fire(
+                        'Deleted!'
+                        , 'Your file has been deleted.'
+                        , 'success'
+                    )
+                }
+            })
+        });
     });
-});
+
 </script>
 <script>
-    $('#image').change(function (e) {
+    $('#image').change(function(e) {
         var reader = new FileReader();
-        reader.onload = function (e) {
+        reader.onload = function(e) {
             $('#showImage').attr('src', e.target.result);
         }
         reader.readAsDataURL(e.target.files['0']);
     });
+
 </script>
 </body>
 </html>
--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -1,11 +1,10 @@
 @php
-    $prefix = Request::route()->getPrefix();
-    $route = Route::current()->getName();
+$prefix = Request::route()->getPrefix();
+$route = Route::current()->getName();
 @endphp
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{route('home')}}" class="brand-link">
-        <img src="{{ asset('/') }}/public/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-             style="opacity: .8">
+        <img src="{{ asset('/') }}/public/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Dashboard</span>
     </a>
     <!-- Sidebar -->
@@ -25,53 +24,53 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
-                @if(Auth::user()->userType == 'admin')
+                {{-- @if(Auth::user()->userType == 'admin')
                 <li class="nav-item has-treeview {{($prefix == '/users') ? 'menu-open' : ''}}">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-address-book"></i>
-                        <p>
-                            Manage Users
-                            <i class="fas fa-angle-left right"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{route('users.view')}}" class="nav-link {{($route == 'users.view') ? 'active' : ''}}">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>View Users</p>
-                            </a>
-                        </li>
-                    </ul>
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-address-book"></i>
+                    <p>
+                        Manage Users
+                        <i class="fas fa-angle-left right"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="{{route('users.view')}}" class="nav-link {{($route == 'users.view') ? 'active' : ''}}">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>View Users</p>
+                        </a>
+                    </li>
+                </ul>
                 </li>
-                @endif
+                @endif --}}
                 <!--mange profile-->
-                <li class="nav-item has-treeview {{($prefix == '/profiles') ? 'menu-open' : ''}}">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-user"></i>
-                        <p>
-                            Manage Profiles
-                            <i class="fas fa-angle-left right"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{route('profiles.view')}}" class="nav-link {{($route == 'profiles.view') ? 'active' : ''}}">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>View Profile</p>
-                            </a>
-                        </li>
-                    </ul>
-                     <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{route('profiles.password.view')}}" class="nav-link {{($route == 'profiles.password.view') ? 'active' : ''}}">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>View Password</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                  <!--mange Purchase-->
-<!--                <li class="nav-item has-treeview {{($prefix == '/purchase') ? 'menu-open' : ''}}">
+                {{-- <li class="nav-item has-treeview {{($prefix == '/profiles') ? 'menu-open' : ''}}">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-user"></i>
+                    <p>
+                        Manage Profiles
+                        <i class="fas fa-angle-left right"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="{{route('profiles.view')}}" class="nav-link {{($route == 'profiles.view') ? 'active' : ''}}">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>View Profile</p>
+                        </a>
+                    </li>
+                </ul>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="{{route('profiles.password.view')}}" class="nav-link {{($route == 'profiles.password.view') ? 'active' : ''}}">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>View Password</p>
+                        </a>
+                    </li>
+                </ul>
+                </li> --}}
+                <!--mange Purchase-->
+                <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-edit"></i>
                         <p>
@@ -81,15 +80,15 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="{{route('purchase.add')}}" class="nav-link {{($route == 'purchase.add') ? 'active' : ''}}">
+                            <a href="" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>add Purchase</p>
                             </a>
                         </li>
                     </ul>
-                </li>-->
+                </li>
             </ul>
-          
+
 
             </ul>
         </nav>
